3f29e4f881b2c0131c1f0d4af25932bf1b316b63
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-09 20:33:14 -0400

    Initial commit

---
+++ src/test/java/com/julie/springboot204/Springboot204ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julie.springboot204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot204ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
